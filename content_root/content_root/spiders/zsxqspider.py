5ad1066e23e8409ddb699cbe04a3a21e6c9f82e7
xdfbb@hotmail.com <xdfbb@hotmail.com>
2021-09-13 23:43:48 +0800

    commit and push

--- content_root/content_root/spiders/zsxqspider.py
+++ content_root/content_root/spiders/zsxqspider.py
@@ -1,17 +1,18 @@
 import scrapy
-from scrapy.spiders import Spider
 
 group_page = "https://wx.zsxq.com/dweb2/index/group/551151485124"
 files_page = 'https://wx.zsxq.com/dweb2/index/files'
+group_files_page = "https://api.zsxq.com/v2/groups/551151485124/files?count=20"
 
 
-class ZsxqSpider(Spider):
+class MySpider(scrapy.Spider):
     name = 'zsxq'
 
+    def start_requests(self):
+        return [scrapy.Request(group_files_page, callback=self.parse_files_page)]
 
-def start_requests(self):
-    return scrapy.Request(group_page, callback=self.parse_files_page)
+    def parse_files_page(self, response):
+        return [scrapy.Request(files_page, callback=self.parse_file_links)]
 
-
-def parse_files_page(self, response):
-    return scrapy.Request(files_page)
+    def parse_file_links(self, response):
+        response.
